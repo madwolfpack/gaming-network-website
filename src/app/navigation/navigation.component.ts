91e8d1e8e2457f302b27b2b7ce31ef1d5843a52a
madwolfpack <zakariegr@gmail.com>
2025-08-01 18:36:41 -0400

    Update navigation.component.ts

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // Import RouterModule
+
 
 @Component({
   selector: 'app-navigation',
   standalone: true, // Make sure this is true
-  imports: [RouterModule], // Add RouterModule to the imports array
+  imports: [CommonModule, RouterModule], // Add RouterModule to the imports array
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
+
 export class NavigationComponent {
-  // You can add any logic for your navigation here
+  navItems = [
+{ icon: 'fa fa-home', name: 'Home', path: '/' },
+{ icon: 'fa fa-user', name: 'Servers', path: '/servers' },
+{ icon: 'fa fa-cog', name: 'Community', path: '/community' },
+{ icon: 'fa fa-cog', name: 'Map', path: 'https://maps.playtsc.com' },
+{ icon: 'fa fa-cog', name: 'Discord', path: '/discord' }
+  ];
 }
